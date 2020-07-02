b40c955625ae1d96be7e15bfa90dee5173866365
Jingjin-Du <510770620@qq.com>
2020-07-03 01:15:54 +0800

    lack two value

--- tutorial/run.py
+++ tutorial/run.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 import time
 import os
-
+import sys
 
 
 # 每n秒执行一次
@@ -13,6 +13,11 @@ def timer(n):
         os.system("scrapy crawl mifeng")
         #os.system("scrapy crawl dmoz")
         time.sleep(5)
+
+        with open('data.out', 'r') as f:
+            out = f.read().split(' ')
+            for it in out:
+                print(it)
 
 
 # 时间
--- tutorial/tutorial/spiders/Mifeng_spiders.py
+++ tutorial/tutorial/spiders/Mifeng_spiders.py
@@ -1,22 +1,11 @@
 import scrapy
 import time
-import run
 
 key_name = {'现价' : 'price', '目前市值排名': 'rank', '换手率' : 'hand', '24H最高' : 'day_max', '24H最低' : 'day_min', '7D最高' : 'week_max', '7D最低' : 'week_min', '24H涨幅' : 'day_add', '7D涨幅' : 'week_add', '今年来' : 'now_year', '24H交易量' : 'day_action_num', '24H交易额' : 'day_action', '24H净流入' : 'day_in'}
 
 key_value = {'price' : '', 'rank': '', 'hand' : '', 'day_max' : '', 'day_min' : '', 'week_max' : '', 'week_min' : '', 'day_add' : '', 'week_add' : '', 'now_year' : '', 'day_action_num' : '', 'day_action' : '', 'day_in' : ''}
 
-'''
-现价  span.price 
-目前排名 div.market-cap
-换手率 
 
-24H涨幅  span.change text-green
-
-24H交易量
-24H交易额  div.item
-24H净流入  div.abs-item
-'''
 class MifengSpider(scrapy.Spider):
     name = "mifeng"
     allowed_domains = ["www.mifengcha.com"]
@@ -29,11 +18,41 @@ class MifengSpider(scrapy.Spider):
     week_ago_url = url + str(week_ago_time) + "&end=" + str(now_time)
     year_url = url + str(year_time) + "&end=" + str(now_time)
     start_urls = [
-        "https://www.feixiaohao.com/currencies/okb/",
-        day_ago_url,
-        week_ago_url,
-        year_url
+        #"https://www.feixiaohao.com/currencies/okb/",
+        day_ago_url,week_ago_url,year_url,
+        "https://www.mifengcha.com/coin/okb"
     ]
+
+    '''
+    现价  span.price 
+    目前排名 div.market-cap
+    换手率 
+
+    24H涨幅  span.change text-green
+
+    24H交易量
+    24H交易额  div.item
+    24H净流入  div.abs-item
+    '''
+
+    def mifengcha(self, response):
+        #价格
+        key_value['price'] = '$' + response.css('span.price::text').extract()[0]
+        #24小时交易额
+        key_value['day_action'] = '$' + response.css('span.value::text').extract()[6]
+        #24小时净流入
+        dayin = response.css('div.abs-item').extract()[6]
+        left = dayin.find('</i>')
+        right = dayin.find('<!---->')
+        key_value['day_in'] = '$' + dayin[left+4 : right]
+        #排名
+        rank = response.css('div.market-cap').css('span')[1].extract()
+        left = rank.find('No.')
+        key_value['rank'] = rank[left+3 : ].split('<')[0]
+
+
+        
+
 
     def feixiaohao(self, response):
         output = {}
@@ -64,7 +83,7 @@ class MifengSpider(scrapy.Spider):
         print(output)
 
 
-    def mifengcha(self, response):
+    def mifengchadata(self, response):
         flag = response.url.split("&")
         start = flag[-2].split("=")[-1]
         end = flag[-1].split("=")[-1]
@@ -92,15 +111,27 @@ class MifengSpider(scrapy.Spider):
             key_value['now_year'] = str(prices_add)+"%"
 
 
+    def construct(self):
+        print(str(key_value))
+        output = ""
+        for key, value in key_name.items():
+            col = key + ":" + key_value[value]
+            output += col+"  "
+        with open('data.out', 'w') as f:
+            f.write(output)
     
     def parse(self, response):
+        
         flag = response.url.split("/")[-2]
-        print(flag)
-        if flag is 'okb':
+        print("<"+flag+">")
+        if flag == "okb":
             self.feixiaohao(response)
-        else:
+        elif flag == "coin":
             self.mifengcha(response)
-        print(key_value)
+        else:
+            self.mifengchadata(response)
+        self.construct()
+        
     
         
                 
